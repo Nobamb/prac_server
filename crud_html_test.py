4f6321f304e1d8e24b746c1227943f44760e65f3
nobamb <jsh12345644[@gmail.com](mailto:you@example.com)>
2026-01-12 15:47:16 +0900

    클로저로 tag 작성

--- crud_html_test.py
+++ crud_html_test.py
@@ -4,9 +4,10 @@
 # 열고 닫는 태그가 하나에 모여있는 태그
 
 # 클로저 진행
+# 한번에 열고 닫는 태그
 def tag(tag_name,types):
     # 이름 지정(은닉 변수)
-    tag_start = f"<{tag_name} "
+    tag_start = f"<{tag_name}"
     
     # type들 추가(클로저)
     def type_plus(tagOpen, types):
@@ -14,7 +15,7 @@ def tag(tag_name,types):
         # 배열을 하나씩 나눔
         for type_name in types:
             # 타입명을 tag에 추가
-            tag += f"{type_name} "
+            tag += f" {type_name}"
         # 닫는 태그 지정
         tag += "/>"
         # tag 반환
@@ -26,11 +27,96 @@ def tag(tag_name,types):
     # tag 출력
     return tag
 
+
+# 열기만 하는 태그
+def open_tag(tag_name, types):
+    # 시작 태그 작성
+    tag_start = f"<{tag_name}"
+    # types를 토대로 클로저 함수 생성
+    def type_plus(tag_start, types):
+        # 시작 태그 지정
+        tag = tag_start
+        # tag에 type들 모두 대입
+        for type_name in types:
+            tag += f" {type_name}"
+        # 모두 더하면 닫기
+        tag += ">"
+        # tag 반환
+        return tag
+    
+    # type_plus 실행
+    tag = type_plus(tag_start, types)
+    # tag return
+    return tag
+    
+    
+    
+# 열고 닫는 태그
+def open_close_tag(tag_name, types, childrens):
+    # 태그 열기
+    tag_start = f"<{tag_name}"
+    
+    # type 추가
+    def type_plus(tag_start, types):
+        # tag 지정
+        tag = tag_start
+        # type들 모두 추가
+        for type_name in types:
+            tag += f" {type_name}"
+        
+        # type들 모두 추가한 후,
+        # 닫아버리기(시작 태그 기준)
+        tag += ">"
+        return tag
+    # 시작 태그 지정
+    open_tag = type_plus(tag_start,types)
+    
+    
+    # 태그 내의 result 지정
+    # children 값들 모두 추가
+    result_init = ""
+    
+    # 배열 childrens을 분해하여
+    # result에 추가
+    def children_plus(result_init, childrens):    
+        # open_tag에 children 배열들 모두 추가
+        # children으로 나눔
+        for children in childrens:
+            # children 추가
+            result_init += children
+        # children 값들 모두 return
+        return result_init
+    
+    # 결과들 대입
+    result = children_plus(result_init, childrens)
+    
+    
+    # close_tag 지정
+    close_tag = f"</{tag_name}>"
+    
+    
+    # 총 태그 지정
+    total_tag = open_tag + result + close_tag
+    
+    # 출력
+    return total_tag
+    
+    
 # meta 태그
 meta = tag("meta",["type1","type2"])
 
 print(meta)
 
+# "!DOCTYPE html" 태그(열기만 함)
+doc_html = open_tag("!DOCTYPE", ["html"])
+
+print(doc_html)
+
+# html 태그
+html = open_close_tag("html",[],[])
+
+# 출력
+print(html)
 
 
 # 필요한 태그
