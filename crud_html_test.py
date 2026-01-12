fdb90cdd57ddbebce83855953a9f6904b4893c5f
nobamb <jsh12345644[@gmail.com](mailto:you@example.com)>
2026-01-12 14:50:07 +0900

    tag 관련 클래스 작성 및 테스트

--- crud_html_test.py
+++ crud_html_test.py
@@ -1,38 +1,83 @@
-# 객체 생성하기
-# html 관련(최상위) 태그
-html = {
-    # doctype지정
-    "doctype": "<!DOCTYPE html>",
-    # html 여는 태그
-    "open": "<html>",
-    # html 닫는 태그
-    "close": "</html>",
-}
+# 클래스 제작
 
-# body 관련 태그
-body = {
-    # body 여는 태그
-    "open": "<body>",
-    # body 닫는 태그
-    "close": "</body>",
-}
+# 열고 닫는 태그가 하나에 모여있는 태그
+class Tag():
+  def __init__(self, tag_name):
+    self.tag = f"<{tag_name} />"
 
-# head 관련 태그
-head = {
-    # head 여는 태그
-    "open": "<head>",
-    # head 닫는 태그
-    "close": "</head>",
-}
+# 여는 태그만 있는 태그
+class OpenTag:
+  # 생성자 생성
+  def __init__(self, tag_name):
+    self.open = f"<{tag_name}>"
 
-# title 관련 태그
+# Tag 오버라이드(닫는 태그 추가)
+class OpenCloseTag(OpenTag):
+  
+  def __init__(self, tag_name):
+     super().__init__(tag_name)
+     self.close = f"</{tag_name}>"
 
-title = {
-    # title 여는 태그
-    "open": "<title>",
-    # title 닫는 태그
-    "close": "</title>",
-}
+
+# 테스트
+# "!DOCTYPE html"
+doctype_html = OpenTag("!DOCTYPE html") 
+
+# 출력
+print(doctype_html.open)
+
+# img
+img = Tag("img")
+
+# 출력
+print(img.tag)
+
+# html
+html = OpenCloseTag("html")
+
+# 출력
+# open
+print(html.open)
+# close
+print(html.close)
+
+
+
+# # 객체 생성하기
+# # html 관련(최상위) 태그
+# html = {
+#     # doctype지정
+#     "doctype": "<!DOCTYPE html>",
+#     # html 여는 태그
+#     "open": "<html>",
+#     # html 닫는 태그
+#     "close": "</html>",
+# }
+
+# # body 관련 태그
+# body = {
+#     # body 여는 태그
+#     "open": "<body>",
+#     # body 닫는 태그
+#     "close": "</body>",
+# }
+
+# # head 관련 태그
+# head = {
+#     # head 여는 태그
+#     "open": "<head>",
+#     # head 닫는 태그
+#     "close": "</head>",
+# }
+
+# # title 관련 태그
+
+# title = {
+#     # title 여는 태그
+#     "open": "<title>",
+#     # title 닫는 태그
+#     "close": "</title>",
+# }
 
 
 
