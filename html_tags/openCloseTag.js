f49ec960a56f71f52935e3f52d0ac266be7fbdaa
nobamb <jsh12345644@gmail.com>
2026-01-13 15:26:30 +0900

    openCloseTag.js 파일 생성 및 구조 생성(주석 작성 포함)

---
+++ html_tags/openCloseTag.js
@@ -0,0 +1,13 @@
+// 열고 닫는 태그를 제작하는 함수
+// 클로저 형식 함수
+// 첫번째 파라마터 : tagName (태그이름)
+// 두번째 파라미터 : types (속성명, 배열형식)
+// 세번째 파라미터 : values (속성값, 배열형식)
+// 네번째 파라미터 ㅣ chidren(태그 내부에 들어가는 내용, 배열 형식)
+
+// types, values, children 모두 기본값 빈 배열
+const openCloseTag = (tagName, types = [], values = [], children=[])=> {
+
+
+
+}
