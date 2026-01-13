f4daff470cb4dedd8324c2f450d17b8751b01a69
nobamb <jsh12345644@gmail.com>
2026-01-13 14:38:31 +0900

    tag.js 통해 tag 제작 함수 생성 및 구조 제작

---
+++ html_tags/tag.js
@@ -0,0 +1,15 @@
+// html 태그를 만들어주는 코드
+// 첫번째 파라미터 : 태그이름
+// 두번째  파라미터 : 속성 이름(배열로 받음)
+// 세번째 파라미터 : 속성 내용(배열로 받음)
+
+const tag = (tagName, types,values) => {
+
+
+
+
+}
+
+
+// export
+export default tag
