686707b9b6562956f78bd199de731417c95c3ee4
nobamb <jsh12345644@gmail.com>
2026-01-13 14:59:49 +0900

    tagStart에서 tag로 이름 변경, 한 태그만 진행하도록 함, types, values에 기본값 []지정

--- html_tags/tag.js
+++ html_tags/tag.js
@@ -5,10 +5,13 @@
 // 첫번째 파라미터 : 태그이름
 // 두번째  파라미터 : 속성 이름(배열로 받음)
 // 세번째 파라미터 : 속성 내용(배열로 받음)
+// types, values 둘 다 기본값 빈 배열
+// 아예 속성을 안넣고 태그를 만들수있기에
 
-const tag = (tagName, types,values) => {
+
+const tag = (tagName, types=[],values=[]) => {
     // 시작 태그
-    let tagStart = `<${tagName}`
+    let tag = `<${tagName}`
 
     // 클로저 함수
     // 속성에 속성값을 추가
@@ -16,21 +19,21 @@ const tag = (tagName, types,values) => {
     
 
         // 속성들을 분해함
-        types.forEach(element,index => {
+        types.forEach((element,index) => {
             
             // 만약에 values가 배열 형식이면서
             // 빈배열이 아닐 때
             if(typeof values === 'object' && values.length > 0){
                 // 속성명에 "="를 붙이면서 
-                // tagStart에 속성명과 속성 값을 넣어줌
-                tagStart += ` ${element}="${values[index]}"`
+                // tag에 속성명과 속성 값을 넣어줌
+                tag += ` ${element}="${values[index]}"`
             }
             // 만약에 values가 배열 형식이면서
             // 빈배열이 아닐 때
             else if(typeof values === 'object' && values.length <= 0){
 
-                // tagStart에 속성명과 속성 값을 넣어줌
-                tagStart += ` ${element}`
+                // tag에 속성명과 속성 값을 넣어줌
+                tag += ` ${element}`
 
             }
 
@@ -38,26 +41,28 @@ const tag = (tagName, types,values) => {
         })
         
         // 태그 마무리
-        tagStart += `>`
+        tag += `/>`
         
     };
     // typePlus 함수 실행
     typePlus(types, values)
 
-    // 태그 닫기
-    tagEnd = `</${tagName}>`
-
     // 최종 태그 반환
-    result = tagStart + tagEnd
 
-    return result;
+    return tag;
 
 
 }
 
+// 태그 생성 테스트
+const testTag1 = tag("div",["class", "id"], ["container","main"])
+// 태그 생성 테스트 2
+const testTag2 = tag("div")
 
 
-
+// 출력
+console.log(testTag1)
+console.log(testTag2)
 
 // export
 export default tag
